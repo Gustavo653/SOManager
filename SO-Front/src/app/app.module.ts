4f712a01175f4e6f74a643c18c7ac06127ba0f9e
Gustavo Henrique da Silva <gustavohs2004@gmail.com>
2022-12-14 19:52:20 -0300

    Adicionando componente de ticket

--- SO-Front/src/app/app.module.ts
+++ SO-Front/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { TicketService } from './demo/service/ticket.service';
+import { TicketComponent } from './demo/components/ticket/ticket.component';
 
 @NgModule({
-    declarations: [AppComponent, NotfoundComponent],
+    declarations: [AppComponent, NotfoundComponent, TicketComponent],
     imports: [AppRoutingModule, AppLayoutModule],
     providers: [
         { provide: LocationStrategy, useClass: HashLocationStrategy },
